8297ea01011c637aa497ff91a02234167b9e6079
clemishow <clement.saulnier@hetic.net>
2017-06-04 16:40:41 +0200

    [FIX] Background color screen unboarding 1

---
+++ StepOneViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StepOneViewController.swift
+//  Teazy
+//
+//  Created by Dev on 04/06/2017.
+//  Copyright © 2017 Dev. All rights reserved.
+//
+
+import UIKit
+
+class StepOneViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(self.view)
+        self.view.backgroundColor = UIColor(red: 47/255, green: 63/255, blue: 83/255, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
