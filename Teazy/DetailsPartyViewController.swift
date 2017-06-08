e6816f37f0118c433b46cceab9828825699de471
clemishow <clement.saulnier@hetic.net>
2017-06-08 05:44:03 +0200

    [UPDATE] Integration of step 1

--- Teazy/DetailsPartyViewController.swift
+++ Teazy/DetailsPartyViewController.swift
@@ -12,12 +12,20 @@ class DetailsPartyViewController: UIViewController, UICollectionViewDelegate, UI
 
     let images = ["profile", "profile", "profile", "profile", "profile", "profile"]
     
+    
     @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var btnLessRow1: UIButton!
+    @IBOutlet weak var btnPlusRow1: UIButton!
+    @IBOutlet weak var btnPlusRow2: UIButton!
+    @IBOutlet weak var btnLessRow2: UIButton!
     @IBOutlet weak var segmentBar: UISegmentedControl!
     @IBOutlet weak var imageProfile: UIImageView!
     @IBOutlet weak var yoyoCollection: UICollectionView!
     
+    @IBOutlet weak var buttonAddNewStuff: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor(red: 55/255, green: 71/255, blue: 92/255, alpha: 1)
@@ -32,6 +40,36 @@ class DetailsPartyViewController: UIViewController, UICollectionViewDelegate, UI
         self.yoyoCollection.delegate = self
         self.yoyoCollection.dataSource = self
         self.yoyoCollection.backgroundColor = UIColor(red: 47/255, green: 63/255, blue: 83/255, alpha: 1)
+        
+        self.buttonAddNewStuff.layer.borderColor = UIColor.white.cgColor
+        self.buttonAddNewStuff.layer.borderWidth = 1
+        self.buttonAddNewStuff.layer.cornerRadius = (self.buttonAddNewStuff.frame.width / 2)
+        self.buttonAddNewStuff.layer.masksToBounds = true
+        self.buttonAddNewStuff.tintColor = .white
+        
+        self.btnLessRow1.layer.borderColor = UIColor.white.cgColor
+        self.btnLessRow1.layer.borderWidth = 1
+        self.btnLessRow1.layer.cornerRadius = (self.buttonAddNewStuff.frame.width / 2)
+        self.btnLessRow1.layer.masksToBounds = true
+        self.btnLessRow1.tintColor = .white
+        
+        self.btnPlusRow1.layer.borderColor = UIColor.white.cgColor
+        self.btnPlusRow1.layer.borderWidth = 1
+        self.btnPlusRow1.layer.cornerRadius = (self.buttonAddNewStuff.frame.width / 2)
+        self.btnPlusRow1.layer.masksToBounds = true
+        self.btnPlusRow1.tintColor = .white
+        
+        self.btnPlusRow2.layer.borderColor = UIColor.white.cgColor
+        self.btnPlusRow2.layer.borderWidth = 1
+        self.btnPlusRow2.layer.cornerRadius = (self.buttonAddNewStuff.frame.width / 2)
+        self.btnPlusRow2.layer.masksToBounds = true
+        self.btnPlusRow2.tintColor = .white
+        
+        self.btnLessRow2.layer.borderColor = UIColor.white.cgColor
+        self.btnLessRow2.layer.borderWidth = 1
+        self.btnLessRow2.layer.cornerRadius = (self.buttonAddNewStuff.frame.width / 2)
+        self.btnLessRow2.layer.masksToBounds = true
+        self.btnLessRow2.tintColor = .white
     }
     
     
